554498ead6e402e562f525e9b427a9e8c7541f92
nimil <nimilgeorgepradeepngp@gmail.com>
2024-06-18 10:43:23 +0530

    completed cli as well

--- cli/cli.go
+++ cli/cli.go
@@ -26,7 +26,7 @@ func main() {
 	flag.StringVar(&cfg.ipAdmin, "a", "admin", "Name of admin")
 	flag.StringVar(&cfg.ipPasswd, "p", "passwd", "Passwd of admin")
 	flag.Parse()
-	cfg.dbName = "temp.db" /////db name/////
+	cfg.dbName = "babel.db" /////db name/////
 
 	if !cfg.isCreate {
 		if _, err := os.Stat(cfg.dbName); err != nil {
